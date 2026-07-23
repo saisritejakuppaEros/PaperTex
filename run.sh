b4b396aa2c91bc8316ac982e4092c99b24e4eedc
saisriteja <kuppasaisriteja@gmail.com>
2026-07-23 13:25:23 +0530

    update the method image

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-& "$env:LOCALAPPDATA\Programs\MiKTeX\miktex\bin\x64\pdflatex.exe" main
+$pdflatex = "$env:LOCALAPPDATA\Programs\MiKTeX\miktex\bin\x64\pdflatex.exe"
+$bibtex = "$env:LOCALAPPDATA\Programs\MiKTeX\miktex\bin\x64\bibtex.exe"
+
+& $pdflatex -interaction=nonstopmode main.tex
+& $bibtex main
+& $pdflatex -interaction=nonstopmode main.tex
+& $pdflatex -interaction=nonstopmode main.tex
