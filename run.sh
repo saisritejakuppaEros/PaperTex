718c9abe65b03d3b3cd2c5f963a850ffda05cd2a
saisriteja <kuppasaisriteja@gmail.com>
2026-07-27 23:38:56 +0530

    update the doc to 14 v1

--- run.sh
+++ run.sh
@@ -1,7 +1,20 @@
-$pdflatex = "$env:LOCALAPPDATA\Programs\MiKTeX\miktex\bin\x64\pdflatex.exe"
-$bibtex = "$env:LOCALAPPDATA\Programs\MiKTeX\miktex\bin\x64\bibtex.exe"
+# $pdflatex = "$env:LOCALAPPDATA\Programs\MiKTeX\miktex\bin\x64\pdflatex.exe"
+# $bibtex = "$env:LOCALAPPDATA\Programs\MiKTeX\miktex\bin\x64\bibtex.exe"
 
-& $pdflatex -interaction=nonstopmode main.tex
-& $bibtex main
-& $pdflatex -interaction=nonstopmode main.tex
-& $pdflatex -interaction=nonstopmode main.tex
+# & $pdflatex -interaction=nonstopmode main.tex
+# & $bibtex main
+# & $pdflatex -interaction=nonstopmode main.tex
+# & $pdflatex -interaction=nonstopmode main.tex
+
+
+cd /Users/test/Documents/paper_work/PaperTex
+
+# Install missing packages (user install, no sudo)
+tlmgr init-usertree
+tlmgr --usermode install silence etoolbox lineno xcolor cite caption kvoptions geometry axessibility orcidlink booktabs placeins cleveref eso-pic xstring accsupp
+
+# Then compile
+pdflatex -interaction=nonstopmode main.tex
+bibtex main
+pdflatex -interaction=nonstopmode main.tex
+pdflatex -interaction=nonstopmode main.tex
